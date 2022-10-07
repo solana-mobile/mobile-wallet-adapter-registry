033a68bfc72b1beb63622f46202d47bdbd3e11a2
Steven Laver <steven.laver@solana.com>
2022-10-07 08:07:12 -0700

    Add CI for wallet registry schema validation

--- validate-entries.sh
+++ validate-entries.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 set -e
-ajv validate --spec=draft2019 -c ajv-formats -s wallet-schema.json -d "entries/*.json"
+npx ajv validate --spec=draft2019 -c ajv-formats -s wallet-schema.json -d "entries/*.json"
